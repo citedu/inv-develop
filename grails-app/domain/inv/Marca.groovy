98e9cfb66c2e88408db450f67871413183e9a0d1
Usuario <usuario@citedu.org>
2011-01-31 16:51:11 -0600

    Mi primer commit

---
+++ grails-app/domain/inv/Marca.groovy
@@ -0,0 +1,14 @@
+package inv
+
+class Marca {
+
+    String descripcion
+
+    static constraints = {
+        descripcion()
+    }
+
+    String toString() {
+        descripcion
+    }
+}
---
+++ test/unit/inv/DescripcionTests.groovy
@@ -0,0 +1,17 @@
+package inv
+
+import grails.test.*
+
+class DescripcionTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
